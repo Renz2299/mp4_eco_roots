3297ca1c2ac2b3ded1ca1150aa4bf4a598d8df10
Renz2299 <karenza.kv@outlook.com>
2024-01-06 18:29:44 +0000

    Added new_arrivals and bundles views and rendered on index.html

--- home/views.py
+++ home/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 
 def index(request):
     """ A view to return the index page """
 
-    return render(request, 'home/index.html')
+    new_arrivals = Product.objects.filter(category='5')
+
+    bundles = Product.objects.filter(category='6')
+
+    context = {
+        'new_arrivals': new_arrivals,
+        'bundles': bundles,
+    }
+
+    return render(request, 'home/index.html', context)
