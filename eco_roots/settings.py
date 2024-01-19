88debd1befde705e2f1549f52d5d0e7e4d3321ca
Renz2299 <karenza.kv@outlook.com>
2024-01-19 18:06:00 +0000

    Fixed database settings setup - hopefully deployed app working now

--- eco_roots/settings.py
+++ eco_roots/settings.py
@@ -129,7 +129,7 @@ WSGI_APPLICATION = 'eco_roots.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
-    DATABASE = {
+    DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
